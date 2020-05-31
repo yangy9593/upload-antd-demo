0d0db9a9add23ce0108089d650a2b74e51cac904
裴飞阳 <yangy9593@163.com>
2020-05-31 13:47:58 +0800

    2020年5月31日13:47:58

---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <a-upload-dragger
+            name="file"
+            :multiple="true"
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            @change="handleChange"
+            :supportServerRender="true"
+        >
+            <p class="ant-upload-drag-icon">
+                <a-icon type="inbox" />
+            </p>
+            <p class="ant-upload-text">点击或拖拽上传文件</p>
+        </a-upload-dragger>
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { Icon, Upload } from "ant-design-vue/lib";
+
+Vue.use(Icon).use(Upload);
+
+export default {
+    data() {
+        return {};
+    },
+    methods: {
+        handleChange(info) {
+            const status = info.file.status;
+            if (status !== "uploading") {
+                console.log(info.file, info.fileList);
+            }
+            if (status === "done") {
+                this.$message.success(
+                    `${info.file.name} file uploaded successfully.`
+                );
+            } else if (status === "error") {
+                this.$message.error(`${info.file.name} file upload failed.`);
+            }
+        }
+    }
+};
+</script>
+
+<style>
+.container{
+  margin: 100px;
+}
+</style>
